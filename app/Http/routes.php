908baecc46b7742b61f13575cd55261652a78cb9
tengyu <fuiboom@outlook.com>
2016-10-17 16:50:33 +0800

    learnlaravel5-0.1

---
+++ app/Http/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::auth();
+Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
+
+Route::get('now', function () {
+    return date("Y-m-d H:i:s");
+});
+Route::group(['middleware' => 'auth', 'namespace' => 'Admin', 'prefix' => 'admin'], function() {
+    Route::get('/', 'HomeController@index');
+});
+Route::group(['middleware' => 'auth', 'namespace' => 'Admin', 'prefix' => 'admin'], function() {
+    Route::get('/', 'HomeController@index');
+    //Route::get('article', 'ArticleController@index');
+    Route::resource('article', 'ArticleController');
+});
+Route::get('article/{id}', 'ArticleController@show');
+Route::post('comment', 'CommentController@store');
